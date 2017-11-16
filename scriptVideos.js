8bd027d5c169567f8720592e7d46be627b0eb2d9
neesmusuns <31275615+neesmusuns@users.noreply.github.com>
2017-11-16 11:55:54 +0000

    adding gogn
    heres sum stuff

---
+++ scriptVideos.js
@@ -0,0 +1,21 @@
+var url = 'videos.json';
+
+function search() {
+  var request = new XMLHttpRequest();
+
+  request.open('GET', url, true);
+  request.onload = function() {
+    var data = JSON.parse(request.responseText);
+    console.log(data);
+
+    var id1 = data.videos[0];
+    var id2 = data.videos[1];
+    var id3 = data.videos[2];
+    var id4 = data.videos[3];
+
+    var catNew = data.categories[0];
+    var catTeach = data.categories[1];
+    var catFun = data.categories[2];
+  };
+  request.send();
+};
