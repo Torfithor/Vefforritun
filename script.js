c3783a228569a0141f0e8e12107b0073d7ba288b
neesmusuns <31275615+neesmusuns@users.noreply.github.com>
2017-11-16 13:18:21 +0000

    demo
    demo

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function search() {
     var id2 = data.videos[1];
     var id3 = data.videos[2];
     var id4 = data.videos[3];
-
+    document.getElementById("demo").innerHTML = id4.title;
     var catNew = data.categories[0];
     var catTeach = data.categories[1];
     var catFun = data.categories[2];
