bbb0ab377d469b85b1f1cad850cf9326adc7403f
Johann Orn <johann.orn@outlook.com>
2017-11-21 13:02:00 +0000

    smá fikt
    for loops í console log

--- script.js
+++ script.js
@@ -11,10 +11,26 @@ function search() {
     var id2 = data.videos[1];
     var id3 = data.videos[2];
     var id4 = data.videos[3];
-    document.getElementById("demo").innerHTML = id4.title;
+
     var catNew = data.categories[0];
     var catTeach = data.categories[1];
     var catFun = data.categories[2];
+/*
+    document.getElementById('catNewTitle').innerHTML = catNew.title;
+    document.getElementById('catTeachTitle').innerHTML = catTeach.title;
+    document.getElementById('catFunTitle').innerHTML = catFun.title;
+*/
+    for (var i=0; i < data.categories.length; i++) {
+      console.log(data.categories[i].title);
+    }
+
+    for (var i=0; i<data.videos.length; i++) {
+      console.log(data.videos[i].title);
+    }
+/*
+    for (var i=0; i<catFun.videos.length; i++) {
+      catFun.videos[i];
+    }*/
   };
   request.send();
 };
