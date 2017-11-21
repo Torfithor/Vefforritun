2cdc8a97eed32d6712ce7860cb63483ed85cf066
Johann Orn <johann.orn@outlook.com>
2017-11-21 17:29:00 +0000

    commit to death

--- script-compiled.js
+++ script-compiled.js
@@ -13,29 +13,32 @@ function fetchJson() {
   request.onload = function () {
 
     if (request.status >= 200 && request.status < 400) {
-      var data = JSON.parse(request.response);
+      var data = JSON.parse(request.responseText);
       videoList = data;
     } else {
       showError('Villa kom upp');
     }
+    console.log(videoList);
+    console.log(videoList.videos[2].created);
   };
   request.send();
-};
 
-console.log(videoList);
-/*
-function time(video) {
-  var now = Date.now();
-  var difference = now - video.videos.created;
-  var days = difference/1000/3600/24;
-  if (days < 7) {
-    return "Fyrir " + days + " dögum síðan";
+  function time(video) {
+    var now = Date.now();
+    var difference = now - video.videos[1].created;
+    var days = difference / 1000 / 3600 / 24;
+    if (days < 7) {
+      return "Fyrir " + days + " dögum síðan";
+    } else {
+      return "Fyrir " + Math.round(days / 7) + " víkum síðan";
+    }
+
+    function showVideoList() {
+      return videoList.videos[0];
+    };
+    console.log(time(videoList));
   }
-  else {
-    return "Fyrir " + Math.round(days/7) + " víkum síðan";
-  }
-}
-console.log(time(videoList));
-*/
+  console.log(time(videoList));
+};
 
 //# sourceMappingURL=script-compiled.js.map
