373652ab2429a9801cfd9c598c8b46ef024fb663
neesmusuns <31275615+neesmusuns@users.noreply.github.com>
2017-11-21 10:54:36 +0000

    trying to make the JSON fetching better
    trying to make the JSON fetching better

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
+var videoList = new Object();
 
-function search() {
+document.addEventListener('DOMContentLoaded', function () {
+  fetchJson();
+
+});
+
+function fetchJson() {
   var request = new XMLHttpRequest();
 
   request.open('GET', './videos.json', true);
   request.onload = function() {
-    var data = JSON.parse(request.responseText);
-    console.log(data);
-
-    var id1 = data.videos[0];
-    var id2 = data.videos[1];
-    var id3 = data.videos[2];
-    var id4 = data.videos[3];
-    document.getElementById("demo").innerHTML = id4.title;
-    var catNew = data.categories[0];
-    var catTeach = data.categories[1];
-    var catFun = data.categories[2];
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.response);
+      videoList = data;
+    } else {
+      showError('Villa kom upp');
+    }
   };
   request.send();
 };
 
-search();
+console.log(videoList);
+/*
+function time(video) {
+  var now = Date.now();
+  var difference = now - video.videos.created;
+  var days = difference/1000/3600/24;
+  if (days < 7) {
+    return "Fyrir " + days + " dögum síðan";
+  }
+  else {
+    return "Fyrir " + Math.round(days/7) + " víkum síðan";
+  }
+}
+console.log(time(videoList));
+*/
