760efe8d3f27d69c7a42c21869d4451aec781618
unarunars <unarunarsdottir@gmail.com>
2017-11-23 11:47:48 +0000

    alskonar

--- script-compiled.js
+++ script-compiled.js
@@ -1,25 +1,106 @@
-'use strict';
+"use strict";
 
-function search() {
+document.addEventListener("DOMContentLoaded", function () {
+  fetchJson();
+});
+
+function fetchJson() {
   var request = new XMLHttpRequest();
 
-  request.open('GET', './videos.json', true);
+  request.open("GET", "./videos.json", true);
   request.onload = function () {
-    var data = JSON.parse(request.responseText);
-    console.log(data);
-
-    var id1 = data.videos[0];
-    var id2 = data.videos[1];
-    var id3 = data.videos[2];
-    var id4 = data.videos[3];
-
-    var catNew = data.categories[0];
-    var catTeach = data.categories[1];
-    var catFun = data.categories[2];
+    var data = JSON.parse(request.response);
+    time(data, 0);
+    myFunction(data.videos[1].video);
   };
   request.send();
-};
+}
 
-search();
+function time(video, n) {
+  var now = Date.now();
+  var difference = now - video.videos[n].created;
+  var days = difference / 1000 / 3600 / 24;
+  if (days < 7) {
+    return "Fyrir " + days + " dögum síðan";
+  } else {
+    return "Fyrir " + Math.round(days / 7) + " víkum síðan";
+  }
+}
+
+function myFunction(videolink) {
+  var x = document.createElement("VIDEO");
+
+  x.setAttribute("src", videolink);
+
+  x.setAttribute("width", "320");
+  x.setAttribute("height", "240");
+  x.setAttribute("controls", "controls");
+  document.body.appendChild(x);
+}
+
+//á button ekki vera í global og föllunum??
+/*  function showVideo() {
+    var divelement = document.createElement("div");
+    var video = document.createElement("video");
+    video.setAttribute("src", "./videos/small.mp4");
+    video.setAttribute("type", "video/mp4");
+    divelement.appendChild(video);
+    //body.appendChild(divelement);
+    console.log(video);
+  }
+  showVideo();*/
+//  var button = document.createElement("button");
+
+function showButton() {
+  var body = document.querySelector("body");
+  var divelement = document.createElement("div");
+  var img = document.createElement("img");
+  img.setAttribute("src", "./img/back.svg");
+  img.setAttribute("src", "./img/fullscreen.svg");
+  img.setAttribute("src", "./img/mute.svg");
+  img.setAttribute("src", "./img/next.svg");
+  img.setAttribute("src", "./img/pause.svg");
+  img.setAttribute("src", "./img/play.svg");
+  img.setAttribute("src", "./img/unmute.svg");
+  divelement.appendChild(img);
+  body.appendChild(divelement);
+}
+showButton();
+
+function PlayPause() {
+  var button = document.createElement("button");
+  if (mediaPlayer.paused || mediaPlayer.ended) {
+    button.title = "pause";
+    button.innerHTML = "pause";
+    button.className = "pause";
+    mediaPlayer.play();
+  } else {
+    button.title = "play";
+    button.innerHTML = "play";
+    btn.className = "play";
+    mediaPlayer.pause();
+  }
+}
+function changeButtonType(button, value) {
+  button.title = value;
+  button.innerHTML = value;
+  button.className = value;
+}
+function stopPlayer() {
+  var buttonStop = document.createElement("button");
+  mediaPlayer.pause();
+  mediaPlayer.currentTime = 0;
+}
+function mute() {
+  var buttonMute = document.createElement("button");
+  if (mediaPlayer.muted) {
+    changeButtonType(button, "mute");
+    mediaPlayer.muted = false;
+  } else {
+    changeButtonType(button, "unmute");
+    mediaPlayer.muted = true;
+  }
+}
+//  request.send
 
 //# sourceMappingURL=script-compiled.js.map
